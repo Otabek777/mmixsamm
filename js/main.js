a8fc42d35c0f88948a4e57aa3220ce7d03919cab
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2023-02-16 07:04:51 +0500

    mobile menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+$(".header__burgir").click(function() {
+    if($(".header__burgir").hasClass("active")) {
+        $(".header__right").removeClass("active");
+        $(".header__burgir").removeClass("rotate");
+        setTimeout(function() {
+            $(".header__burgir").removeClass("active");
+        }, 300);
+    } else {
+        $(".header__right").addClass("active");
+        $(".header__burgir").addClass("active");
+        setTimeout(function() {
+            $(".header__burgir").addClass("rotate");
+        }, 300);
+    }
+    
+});
 if($(".form_input")) {
     $(".form_input input").focus(function() {
         $(this).parent().addClass("open");
